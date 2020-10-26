1a40f487b8b50a817db818544dd9a02ea9f26906
jullierosa <julliecdsr@gmail.com>
2020-10-26 07:31:24 -0300

    Review

--- Exerc_Javascript/desafio_boas_vindas/script.js
+++ Exerc_Javascript/desafio_boas_vindas/script.js
@@ -1,6 +1,7 @@
 function clicar(){
     var desafio = document.querySelector("div#desafio")
-    var nome = prompt("Qual é o seu nome?")
+    var res = document.querySelector("div#res")
+    var nome = prompt("Antes de qualquer coisa, qual é o seu nome?")
     var idade = prompt(`Olá ${nome}! Qual sua idade?`)
-    alert(`Acabei de conhecer ${nome} que tem ${idade} anos de idade`)
+    res.innerHTML = `Acabei de conhecer ${nome} que tem ${idade} anos de idade`
 }
--- Exerc_Javascript/desafio_salario_funcionario/script.js
+++ Exerc_Javascript/desafio_salario_funcionario/script.js
@@ -9,10 +9,10 @@ function verificar (){
     var res1 = parseFloat(txtn2 * 0.03)
     var res2 = parseFloat(txtn2 * 0.12)
     var res3 = parseFloat(txtn2 * 0.20)
-    if (txtn3 < 3){
+    if (txtn3 <= 3){
         var resf = res1 + txtn2
         res.innerHTML = `<i>${txtn1}</i>, o seu salário era <b>${txtn2} reais </b> e passou para <b>${resf} reais</b> devido ao seu tempo de casa que é de <i>${txtn3} anos</i> e você ganhou um aumento de 3%`
-    }else if (txtn3 < 10){
+    }else if (txtn3 <= 10){
         var resf1 = res2  + txtn2
         res.innerHTML = `<i>${txtn1}</i>, o seu salário era <b>${txtn2} reais </b> e passou para <b>${resf1} reais</b> devido ao seu tempo de casa que é de <i>${txtn3} anos</i> e você ganhou um aumento de 12%`
     }else{
