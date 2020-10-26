18472786e1984a84709d55418bf31b3d0cac2a0c
jullierosa <julliecdsr@gmail.com>
2020-10-26 07:54:40 -0300

    Review

--- Exerc_Javascript/desafio_calculadora_troco/script.js
+++ Exerc_Javascript/desafio_calculadora_troco/script.js
@@ -1,10 +1,12 @@
-var nomes = prompt("Antes de qualquer coisa, qual é o seu nome?")
-alert(`Olá ${nomes}!`)
+var nome = prompt("Antes de qualquer coisa, qual é o seu nome?")
+alert(`Olá ${nome}!`)
 function calcular(){
     var calc = document.querySelector("div#calc")
-    var nome = prompt(`${nomes}, qual é o(a) nome do produlto que você está comprando?`)
-    var preco = prompt(`Quanto custa o(a) ${nome} que está comprando?`)
-    var valor = prompt(`Qual valor você deu para pagar o(a) ${nome}`)
+    var res = document.querySelector("div#res")
+    var prod = prompt(`${nome}, qual é o(a) nome do produlto que você está comprando?`)
+    var preco = prompt(`Quanto custa o(a) ${prod} que está comprando?`)
+    var valor = prompt(`Qual valor você deu para pagar o(a) ${prod}`)
     var resul = Number(valor - preco)
-    var fim = alert(`${nomes}, você comprou o(a) ${nome} que custou R$:${preco},00. Você deu R$: ${valor},00 em dinheiro e receberá de troco o valor de R$: ${resul},00 reais. ${nomes}, Obrigada e volte sempre!`)
+    var fim = alert(`${nome}, você comprou o(a) ${prod} que custou R$:${preco}. Você deu R$: ${valor},00 em dinheiro e receberá de troco o valor de R$: ${resul} reais.`)
+    res.innerHTML = `<b>${nome}</b> obrigada e volte sempre!`
 }
--- Exerc_Javascript/desafio_conversor_de_moedas/script.js
+++ Exerc_Javascript/desafio_conversor_de_moedas/script.js
@@ -7,5 +7,5 @@ function converter(){
     var respf = parseFloat(Number(dim/Cdol))
     var respf2 = parseFloat(Number(dim/Ceuro))
     res.innerHTML = (`Você conseguirá comprar <b>US$ ${respf} dolares</b> com <b>R$ ${dim},00 reais</b> que você tem na sua carteira, considerando a cotação do dolár que atualmente está a <b>US$ ${Cdol}dolares</b>.</br></br>`)
-    res.innerHTML += (`E você também conseguirá comprar <b>E${respf2} euros</b> com <b>R$ ${dim},00 reais</b> que você tem na sua carteira, considerando também a cotação do euro que atualmente está a <b>E${Ceuro}euros</b>.<i>Obrigada</i>`)
+    res.innerHTML += (`E você também conseguirá comprar <b>E${respf2} euros</b> com <b>R$ ${dim},00 reais</b> que você tem na sua carteira, considerando também a cotação do euro que atualmente está a <b>E${Ceuro}euros</b>.</br><i>Obrigada</i>`)
 }
