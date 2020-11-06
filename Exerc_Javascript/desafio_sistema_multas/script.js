4df8b4a17688c6cdcdce5113506935c2b11b2c73
jullierosa <julliecdsr@gmail.com>
2020-11-06 05:33:23 -0300

    exercícios adicionados

---
+++ Exerc_Javascript/desafio_sistema_multas/script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    var tnom = document.querySelector("input#tnom")
+    var tvel = document.querySelector("input#tvel")
+    var res = document.querySelector("div#res")
+    var tnom = String(tnom.value)
+    var tvel = Number(tvel.value)
+    if (tvel <= 60){
+        res.innerHTML = `${tnom}, sua velocidade foi de ${tvel}km/h.<br><br>`
+    } else {
+        var s = parseFloat((tvel-60)*5)
+        res.innerHTML = `${tnom}, você pagará ${s} reais de multa devido a ultrapassagem do limite de velocidade de <b>60km/h</b>`
+    }
+    res.innerHTML += `<b><br>Obrigada pela atenção!USE SEMPRE CINTO DE SEGURANÇA</b>`
+}
