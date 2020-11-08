1efc5c09eb5ff5f8693a42d0358ebd3d46c55bd0
jullierosa <julliecdsr@gmail.com>
2020-11-08 10:30:36 -0300

    commit

--- Exerc_Javascript/desafio_boas_vindas/script.js
+++ Exerc_Javascript/desafio_boas_vindas/script.js
@@ -3,5 +3,5 @@ function clicar(){
     var res = document.querySelector("div#res")
     var nome = prompt("Antes de qualquer coisa, qual é o seu nome?")
     var idade = prompt(`Olá ${nome}! Qual sua idade?`)
-    res.innerHTML = `Acabei de conhecer ${nome} que tem ${idade} anos de idade`
+    res.innerHTML = `<p>Acabei de conhecer ${nome} que tem ${idade} anos de idade</p>`
 }
