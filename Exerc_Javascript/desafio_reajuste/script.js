25b43cbc35f2d04f7155e26c92bf299a3959b6fc
jullierosa <julliecdsr@gmail.com>
2020-11-08 10:49:43 -0300

    commit

--- Exerc_Javascript/desafio_reajuste/script.js
+++ Exerc_Javascript/desafio_reajuste/script.js
@@ -7,7 +7,7 @@ function calcular(){
     var Vporc = parseFloat(porc/100)
     var val = parseFloat(salar*Vporc)
     var vf = parseFloat(Number(salar)+val)
-    res.innerHTML = (`<b>${nome} recebeu um aumento salarial</b></br>`)
+    res.innerHTML = (`<b>${nome} recebeu um aumento salarial de ${porc}%</b></br>`)
     res.innerHTML += (`O salário atual era <b>R$${salar},00</b></br>`)
     res.innerHTML += (`Com um aumento de <b>${porc}%</b>, o salário vai aumentar <b>R$${val},00</b> no próximo mês.</br>`)
     res.innerHTML += (`E a partir daí, <b>${nome}</b> vai passar a ganhar <b>R$${vf},00 reais</b>`)
