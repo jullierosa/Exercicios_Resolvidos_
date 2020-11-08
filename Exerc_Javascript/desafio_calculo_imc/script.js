c318e742e6a156757fab9f3e8d8dd1260006a30f
jullierosa <julliecdsr@gmail.com>
2020-11-08 10:40:07 -0300

    commit

--- Exerc_Javascript/desafio_calculo_imc/script.js
+++ Exerc_Javascript/desafio_calculo_imc/script.js
@@ -8,15 +8,15 @@ function calcular(){
     var talt = Number(talt.value)
     var imc = parseFloat(tpeso/talt**2)
     if (imc < 18.5){
-        res.innerHTML = `${tnom}, você está <b>abaixo do peso</b><br>`
+        res.innerHTML = `<p>${tnom}, você está <b>abaixo do peso</b></p><br>`
     }else if (imc < 25){
-        res.innerHTML = `${tnom}, você esta no seu <b>peso ideal</b><br>`
+        res.innerHTML = `<p>${tnom}, você esta no seu <b>peso ideal</b></p><br>`
     }else if (imc < 30){
-        res.innerHTML = `${tnom}, você está com <b>sobrepeso</b><br>`
+        res.innerHTML = `<p>${tnom}, você está com <b>sobrepeso</b></p><br>`
     }else if (imc < 40){
-        res.innerHTML = `${tnom}, voce esta com <b>obesidade</b><br>`
+        res.innerHTML = `<p>${tnom}, voce esta com <b>obesidade</b></p><br>`
     }else {
-        res.innerHTML = `${tnom}, você está com <b>obesidade mórbida</b><br>`
+        res.innerHTML = `<p>${tnom}, você está com <b>obesidade mórbida</b></p><br>`
     }
-    res.innerHTML += `<b>NÃO ESQUEÇA DE SE CUIDAR</b>`
+    res.innerHTML += `<p><b>NÃO ESQUEÇA DE SE CUIDAR</b></p>`
 }
