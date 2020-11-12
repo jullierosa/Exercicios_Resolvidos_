041ad48f7696691c77e3e2b1178e5a7bdef999f0
jullierosa <julliecdsr@gmail.com>
2020-11-12 10:37:59 -0300

    new commit

--- Exerc_Javascript/desafio_botao/script.js
+++ Exerc_Javascript/desafio_botao/script.js
@@ -3,3 +3,8 @@ function criarParagrafo(){
     para.textContent = 'Você clicou no botão!';
     document.body.appendChild(para);
 }
+var botoes = document.querySelectorAll('button');
+
+for (var i = 0; i < botoes.length; i++){
+    botoes[i].addEventListener('click', criarParagrafo);
+}
