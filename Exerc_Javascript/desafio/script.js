c6ca24e6853765ca2b5b795d4a9ff175abbe0233
jullierosa <julliecdsr@gmail.com>
2020-11-13 16:30:19 -0300

    commit

--- Exerc_Javascript/desafio/script.js
+++ Exerc_Javascript/desafio/script.js
@@ -1,12 +1,14 @@
 function calcular(){
-    var txti = document.querySelector("input#txti")
+    //var txti = document.querySelector("input#txti")
     var txtf = document.querySelector("input#txtf")
     var txtp = document.querySelector("input#txtp")
     var res = document.querySelector("div#res")
-    var txti = Number(txti.value)
+    var tit = document.querySelector("div#tit")
+    //var txti = Number(txti.value)
     var txtf = Number(txtf.value)
     var txtp = Number(txtp.value)
-    for (var c= txti; c <= txtf; c++){
-       res.innerHTML = `${c}`
+    while(c <= txtf){
+        res.innerHTML = `passo ${c}`
+        c = c + txtp
     }
 }
