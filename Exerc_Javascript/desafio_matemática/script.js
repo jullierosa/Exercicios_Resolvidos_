455c5c535d88fceb1ae0bbf3d3ea5cbe85024e1c
jullierosa <julliecdsr@gmail.com>
2020-11-14 05:17:16 -0300

    validação

--- Exerc_Javascript/desafio_matemática/script.js
+++ Exerc_Javascript/desafio_matemática/script.js
@@ -1,4 +1,13 @@
 var txtn1 = document.querySelector("input#txtn1")
 var txtn2 = document.querySelector("input#txtn2")
 var res = document.querySelector("div#res")
-//var mat = prompt("Você quer somar, subtrair, multiplicar ou dividir?")
+var mat = prompt("Você quer somar, subtrair, multiplicar ou dividir?")
+function calcular(){
+    if (txtn1.value.length == 0 || txtn2.value.length == 0){
+        alert("ERRO, FALTAM DADOS!")
+    } else {
+        if(mat == "somar"){
+            alert("ok!")
+        }
+    }
+}
