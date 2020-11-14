b959ece11fb754e58b4e8e3fb5628717792a91d6
jullierosa <julliecdsr@gmail.com>
2020-11-14 05:06:42 -0300

    new commit

--- Exerc_Javascript/desafio_matemática/script.js
+++ Exerc_Javascript/desafio_matemática/script.js
@@ -1,1 +1,4 @@
-var mat = prompt("Você quer somar, subtrair, multiplicar ou dividir?")
+var txtn1 = document.querySelector("input#txtn1")
+var txtn2 = document.querySelector("input#txtn2")
+var res = document.querySelector("div#res")
+//var mat = prompt("Você quer somar, subtrair, multiplicar ou dividir?")
