de754169bb34a1eeaec03dfa6ae9b9b9257c910b
jullierosa <julliecdsr@gmail.com>
2020-11-15 10:06:59 -0300

    atualizado

--- Exerc_Javascript/desafio_calcular_desconto/script.js
+++ Exerc_Javascript/desafio_calcular_desconto/script.js
@@ -6,7 +6,7 @@ function calcular(){
     var porc = parseFloat(preco*0.10)
     var des = parseFloat(preco - porc)
     res.innerHTML = (`O preço original era <b>R$${preco},00 reais</b> </br>`)
-    res.innerHTML += (`Você acaba de ganhar <b>R$${porc},00 reais de desconto</b> que é de <b>10%</b> </br>`)
+    res.innerHTML += (`Você acaba de ganhar <b>R$${porc},00 reais de desconto</br>`)
     res.innerHTML += (`No fim, você vai pagar <b>R$${des},00 reais</b> no produto comprado, o(a) <b>${nome}</b>.`)  
      
 }
