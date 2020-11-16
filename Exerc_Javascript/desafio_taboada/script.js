e52b716a17f0faeba2d8273c1dc6917f90b4445d
jullierosa <julliecdsr@gmail.com>
2020-11-16 11:03:33 -0300

    commit

--- Exerc_Javascript/desafio_taboada/script.js
+++ Exerc_Javascript/desafio_taboada/script.js
@@ -1,10 +1,9 @@
 //var num = prompt("Quer saber a taboada de qual número?")
+//alert('Passe o mouse por cima da palavra TABOADA')
 var tab = document.querySelector("div#tabMat")
-var bot = document.querySelector("p#bot")
 var res = document.querySelector("div#res")
 tab.addEventListener('mouseenter' , entrar)
 tab.addEventListener('mouseout' , sair)
-bot.addEventListener('click' , clicar)
 
     function entrar(){
         tab.innerHTML = `Calcularemos entao a Taboada: ${num}`
@@ -12,9 +11,10 @@ bot.addEventListener('click' , clicar)
     function sair(){
         tab.innerHTML = "Que tal tentar resolver a taboada mentalmente primeiro?"
     }
-    function clicar(){
-        for (var cont=0; cont <= 10; cont ++){
-            var cal = Number(num*cont)
-            res.innerHTML = `${num} x ${cont} = ${cal}`
+
+    function calcular(){
+        for (c=1; c <= 10; c++){
+            var resp = 9*c
+            res.innerHTML = `9x ${c} = ${resp}`
         }
     }
