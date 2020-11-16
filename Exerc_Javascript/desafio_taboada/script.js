256bff53e071132bd52eb617c0576f48d197ff0a
jullierosa <julliecdsr@gmail.com>
2020-11-16 09:00:26 -0300

    commit

--- Exerc_Javascript/desafio_taboada/script.js
+++ Exerc_Javascript/desafio_taboada/script.js
@@ -1,6 +1,15 @@
-const tab = document.querySelector('p')
+const tab = document.querySelector('p#tabMat')
+var res = document.querySelector("div#res")
 tab.addEventListener('click' , atualizarTab)
     function atualizarTab(){
         var num = prompt("Quer saber a taboada de qual número?")
         tab.textContent = 'Taboada: ' + num
+        function inicio() {
+            var cont = 0
+            var lim = 10
+                while(cont <= 10){
+                    var resp = num * cont
+                    
+                }
+        }
     }
