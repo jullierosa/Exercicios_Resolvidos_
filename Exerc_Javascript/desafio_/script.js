d436cf3e71b62d56bc4fc373926a0b61559198d4
jullierosa <julliecdsr@gmail.com>
2020-11-24 05:21:35 -0300

    exerc atualizado

---
+++ Exerc_Javascript/desafio_/script.js
@@ -0,0 +1,8 @@
+function calcular(){
+    let txtn1 = document.querySelector("input#txtn1")
+    let txtn2 = document.querySelector("input#txtn2")
+    let txtn3 = document.querySelector("input#txtn3")
+    let res = document.querySelector("div#res")
+    
+
+}
