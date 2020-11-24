d6f758850a09b5bfe4569da25b22271f3d4fef93
jullierosa <julliecdsr@gmail.com>
2020-11-24 05:28:14 -0300

    exerc

--- Exerc_Javascript/desafio_/script.js
+++ Exerc_Javascript/desafio_/script.js
@@ -1,8 +1,9 @@
+alert("Digite o valor 1,2 e 3 e calcule o valor de Delta")
 function calcular(){
     let txtn1 = document.querySelector("input#txtn1")
     let txtn2 = document.querySelector("input#txtn2")
     let txtn3 = document.querySelector("input#txtn3")
     let res = document.querySelector("div#res")
-    
+    let resp = Number(txtn2**-4)
 
 }
