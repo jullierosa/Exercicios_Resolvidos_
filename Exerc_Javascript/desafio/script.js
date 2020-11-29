35f5b581e56713ef9b7a6489330d9a63848c46c1
jullierosa <julliecdsr@gmail.com>
2020-11-29 11:27:27 -0300

    exerc erro

--- Exerc_Javascript/desafio/script.js
+++ Exerc_Javascript/desafio/script.js
@@ -1,6 +1,6 @@
 function calcular(){
-    switch(Somador){
-        
-    }
+    var cont = 0
+    while (cont <= 5)
+    var s = (txtn1+txtn2+txtn3+txtn4+txtn5)
     res.innerHTML = `A soma dos valores é ${s}`
 }
