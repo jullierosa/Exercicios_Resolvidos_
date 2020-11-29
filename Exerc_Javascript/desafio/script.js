a904bd86d49b8f25868c42d1c43593e014bc0ec9
jullierosa <julliecdsr@gmail.com>
2020-11-29 09:47:44 -0300

    exerc

--- Exerc_Javascript/desafio/script.js
+++ Exerc_Javascript/desafio/script.js
@@ -1,10 +1,6 @@
 function calcular(){
-    var cont = 1
-    var s = 0
-    while (cont <= 5){
-      var num = prompt("Digite um número: ")
-      var s = (s + num)
-      cont = cont + 1
+    switch(Somador){
+        
     }
     res.innerHTML = `A soma dos valores é ${s}`
 }
