0e9defa88de1b5ed77f6371aba577d6c2805a825
jullierosa <julliecdsr@gmail.com>
2021-07-21 03:45:12 -0300

    atualiz...

--- Exerc_Javascript/desafio_calculadora_troco/script.js
+++ Exerc_Javascript/desafio_calculadora_troco/script.js
@@ -5,7 +5,7 @@ function calcular(){
     var res = document.querySelector("div#res")
     var prod = prompt(`${nome}, qual é o produlto que você está comprando?`)
     var preco = prompt(`Quanto custa o(a) ${prod} que está comprando?`)
-    var valor = prompt(`Qual valor você deu para pagar o(a) ${prod}`)
+    var valor = prompt(`Qual valor que você deu para pagar o(a) ${prod}`)
     var resul = Number(valor - preco)
     var fim = alert(`${nome}, você comprou o(a) ${prod} que custou R$:${preco}. Você deu R$: ${valor},00 em dinheiro e receberá de troco o valor de R$: ${resul} reais.`)
     res.innerHTML = `<p><b>${nome}</b> obrigada e volte sempre!</p>`
